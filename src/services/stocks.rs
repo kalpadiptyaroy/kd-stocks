7598f27816f8e869d60633b3b5cc47090f500df9
Kumar Kalpadiptya Roy <kalpadiptya.roy@outlook.com>
2023-12-12 22:57:31 +0530

    Shifted code from routes to services

--- src/services/stocks.rs
+++ src/services/stocks.rs
@@ -1,7 +1,19 @@
-use rocket::serde::json::{Json};
+use diesel::result::Error;
+use diesel::{RunQueryDsl, QueryDsl};
 
-use crate::routes::stocks::Stock;
+use crate::schema::stock::dsl::*;
+use crate::models::Stock;
+use crate::database;
 
-pub fn get_stock() -> Stock {
+pub fn get_all_stock() -> Vec<Stock> {
+    let connection = &mut database::establish_connection();
     
+    let result = stock.load::<Stock>(connection).expect("Error loading stocks");
+    return result;
+}
+
+pub fn get_stock_by_id(stock_id: i32) -> Result<Stock, Error> {
+    let connection = &mut database::establish_connection();
+    let result: Result<Stock, Error> = stock.find(stock_id).first::<Stock>(connection);
+    return result;
 }
